656c70d85a1c4f91fb5962e9039b3db0fe21a1bf
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-03-05 11:24:37 +0900

    add setInterval for realtime

--- clock.js
+++ clock.js
@@ -6,11 +6,16 @@ function getTime() {
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
   getTime();
+
+  // getTIme 호출 후 1초뒤부터 interval
+  setInterval(getTime, 1000);
 }
 
 init();
