ab0b8cc3dafae32c8e7f2d72c5c6afd781d28a9e
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-03-06 16:26:20 +0900

    add loadName, paintGreeting for currentUser

---
+++ greeting.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    // 처음 유저
+  } else {
+    // 유저가 있는 경우
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
