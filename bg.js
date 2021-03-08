65ddb00b8dab5ed35954a058ddd6c078aeff9524
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-03-08 20:31:01 +0900

    add image background

---
+++ bg.js
@@ -0,0 +1,23 @@
+const bgBody = document.querySelector("body");
+
+const IMG_NUMBER = 3;
+
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `images/${imgNumber + 1}.jpg`;
+  image.classList.add("bgImage");
+  // body의 바로 하위에 image 삽입
+  bgBody.prepend(image);
+}
+
+function getRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function init() {
+  const randomNumber = getRandom();
+  paintImage(randomNumber);
+}
+
+init();
